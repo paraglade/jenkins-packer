a46df1af0e9367ce886c06146b442603736e49df
Nelson Roberts <nroberts@shutterfly.com>
2016-12-14 15:09:36 -0700

    Jenkinsfile testing

--- scripts/audit.sh
+++ scripts/audit.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
 
 echo "@@Installed packages:"
 
 dpkg -l
 
+echo "@LSB_DISTRIBUTION: $(lsb_release -si)"
 echo "@LSB_RELEASE: $(lsb_release -sr)"
 echo "@LSB_CODENAME: $(lsb_release -sc)"
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-sleep 10
+#!/bin/bash -e
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y redis-server nginx php
