ea567af3698aaa6df55841156b54e4c13b0ed75e
Nelson Roberts <nroberts@shutterfly.com>
2016-12-16 15:34:31 -0700

    testing framework

---
+++ tests/spec/localhost/default_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe package('nginx'), :if => os[:family] == 'ubuntu' do
+  it { should be_installed }
+end
+
+describe service('nginx'), :if => os[:family] == 'ubuntu' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe package('php7.0-fpm'), :if => os[:family] == 'ubuntu' do
+  it { should be_installed }
+end
+
+describe service('php-fpm7.0-fpm'), :if => os[:family] == 'ubuntu' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe package('redis-server'), :if => os[:family] == 'ubuntu' do
+  it { should be_installed }
+end
+
+describe service('redis-server'), :if => os[:family] == 'ubuntu' do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe port(6379) do
+  it { should be_listening }
+end
