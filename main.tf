d5c53978d372605bccd74025211da4229400c283
Nelson Roberts <nroberts@shutterfly.com>
2016-12-16 15:35:31 -0700

    terraform goodness

---
+++ main.tf
@@ -0,0 +1,66 @@
+# Specify the provider and access details
+provider "aws" {
+  region = "${var.aws_region}"
+  shared_credentials_file  = "/home/nroberts/.aws/credentials"
+  profile                  = "thislife-dev"
+
+}
+
+resource "aws_security_group" "jenkins-tester" {
+  name        = "jenkins-tester"
+  description = "jenkins-tester"
+  tags = "${var.aws_tags}"
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "auth" {
+  key_name   = "${var.key_name}"
+  public_key = "${file(var.public_key_path)}"
+}
+
+resource "aws_instance" "jenkins-build-test" {
+  connection {
+    user = "ubuntu"
+  }
+
+  instance_type = "t2.micro"
+
+  ami = "${lookup(var.aws_amis, var.aws_region)}"
+
+  key_name = "${aws_key_pair.auth.id}"
+
+  vpc_security_group_ids = ["${aws_security_group.jenkins-tester.id}"]
+
+  tags = "${var.aws_tags}"
+
+  # We run a remote provisioner on the instance after creating it.
+  # In this case, we just install nginx and start it. By default,
+  # this should be on port 80
+  provisioner "file" {
+        source = "tests"
+        destination = "/tmp"
+    }
+  provisioner "remote-exec" {
+    inline = [
+      "sleep 30",
+      "sudo apt-get -y update",
+      "sudo apt-get -y install ruby-serverspec",
+      "cd /tmp/tests && rake"
+    ]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "public_key_path" {
+  description = <<DESCRIPTION
+Path to the SSH public key to be used for authentication.
+Ensure this keypair is added to your local SSH agent so provisioners can
+connect.
+
+Example: ~/.ssh/terraform.pub
+DESCRIPTION
+}
+
+variable "key_name" {
+  description = "Desired name of AWS key pair"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-west-1"
+}
+
+variable "aws_amis" {
+  default = {
+    us-west-1 = "ami-65b8ee05"
+  }
+}
+
+variable "aws_tags" {
+  description = "AWS tags"
+  default = {
+    Name = "jenkings-testing"
+    User = "nroberts"
+  }
+}
